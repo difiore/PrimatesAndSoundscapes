fb8ca0769ff2a2514ad8cb0b95f169a5911a1ec7
Callicebus <silvyvankuijk@gmail.com>
2022-03-11 14:13:23 -0600

    Adding ambient noise measurements R files 2022-03-11

---
+++ AmbientNoise.R
@@ -0,0 +1,6 @@
+#Ambient Noise Measurements
+#Author(s): Silvy van Kuijk
+
+setwd('C:/Users/Silvy/Documents/R/Repos/Project3WeatherPatterns')
+source("tonyPAMGuide_Meta_revised_AD2.R")
+window_length <- 600 # new variable to define the length of the window of time being looked at

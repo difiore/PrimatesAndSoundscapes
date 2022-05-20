fa870ee6702174c2f75158eea2f5098c1603988d
Sun O'Brien <suncs@utexas.edu>
2022-05-20 16:20:56 -0500

    scripts now exclude leftjoin, fixed original detection cutoff script to work with new process_output_files.R

--- Scripts/Process_Output_Files.R
+++ Scripts/Process_Output_Files.R
@@ -47,7 +47,7 @@ for (f in filenames){
       folder <- locationSubstrings[[1]][1]
       location <- locationSubstrings[[1]][2]
       recording.date <- locationSubstrings[[1]][3]
-      audiofile <- locationSubstrings[[1]][4]
+      audiofile_name <- locationSubstrings[[1]][4]
       filename <- paste0(fname)
     }
     l <- str_split(d[i], "[ ]+")
@@ -55,6 +55,7 @@ for (f in filenames){
     if (str_detect(l[[1]][1], "F") ==TRUE){
       index <- index + 1
       survey_file_name[index] <- filename
+      audiofile[index] <- audiofile_name
       template[index] <- l[[1]][1]
       min.score[index] <- as.numeric(l[[1]][2])
       max.score[index] <- as.numeric(l[[1]][3])
@@ -105,19 +106,13 @@ results <- results[, c(13, 10, 11, 12, 3, 4, 5, 6, 7, 8, 9)]
 #Problems are about to appear, so let's gather some extra data:
 str(results) #This shows us the 'audiofile' column is a string.
 
-#Load in next file. The file 'PrimatesInFiles' notes which audio files had
-  # primate calls found by manual observers. NOTE: Not every line in this file
-  # is a unique recording. There are duplicates where a recording had multiple
-  # titi and/or howler calls in it.
-PrimatesInFiles <- read_csv("Primates_In_All_Recordings.csv", col_names = TRUE)
-str(PrimatesInFiles)
-# 'audiofile' again seems to be a character.
-
-
 # Here's the problem: merging these tables does not lead to the desired results.
-Merged_tables <- left_join(results, PrimatesInFiles, by = c("audiofile" = "audiofile"))
+#Merged_tables <- left_join(results, PrimatesInFiles, by = c("audiofile" = "audiofile"))
 
-fwrite(Merged_tables, "consolidated_completedata_output.csv") #The usual write_csv() only wrote the first 600 lines rather than all 14000+!
+# fwrite(Merged_tables, "consolidated_completedata_output.csv") #The usual write_csv() only wrote the first 600 lines rather than all 14000+!
+
+# temporarily not saving join of manual data
+fwrite(results, "consolidated_completedata_output.csv") #The usual write_csv() only wrote the first 600 lines rather than all 14000+!
 
 # cleanup memory and work space
 rm(list=c("results", "filenames","f"))
--- Scripts/run_detection_cutoff.R
+++ Scripts/run_detection_cutoff.R
@@ -4,16 +4,20 @@ library(ggplot2)
 setwd("C:/Users/ao25848/Documents/PrimatesAndSoundscapes")
 
 # pull in processed output files from template run
-template_output <- read_csv("consolidated_output_ampcut25.csv", show_col_types = FALSE, skip_empty_rows = TRUE)
+template_output <- read_csv("consolidated_completedata_output.csv", show_col_types = FALSE, skip_empty_rows = TRUE)
 
 # create output tibble for results
 results <- tibble(
   original_output_file=character(),
   detection=character(),
-  avg_titi_max_score=numeric())
+  avg_titi_max_score=numeric(),
+  avg_howler_max_score=numeric(),
+  confusion_matrix_number=numeric()
+  )
 
 line_count = nrow(template_output) + 1     # for some reason it excludes the first row. add 1.
 num_files = line_count / 12     # we use twelve templates at the moment
+print(paste("num_files ==", num_files))
 
 # loop by file name
     # loop by template number
@@ -29,44 +33,54 @@ while(file_num <= num_files) { # 80 files
     original_output_file=character(),
     detection=character(),
     avg_titi_max_score=numeric())
-  
+
   original_output_file <- character()
   detection <- character()
   avg_titi_max_score <- numeric()
-  
-  file_name = template_output[[5]][line_num]
+
+  file_name = template_output[[1]][line_num]
 
   for(x in 1:12) { # 12 templates/file
-    print(template_output[[5]][line_num])
-    if(template_output[[9]][line_num] == "T") {
-      #print("titi")
-      titi_template_sum = titi_template_sum + template_output[[11]][line_num]
+    print(template_output[[1]][line_num])
+    if(template_output[[8]][line_num] == "T") {
+      print(paste("titi", template_output[[10]][line_num]))
+      titi_template_sum = titi_template_sum + template_output[[10]][line_num]
     } else { # howler template
-      #print("howler")
-      howler_template_sum = howler_template_sum + template_output[[11]][line_num]
+      print(paste("howler", template_output[[10]][line_num]))
+      howler_template_sum = howler_template_sum + template_output[[10]][line_num]
     }
     line_num = line_num + 1
   }
-  
+
   titi_template_sum = titi_template_sum / 10      # 10 titi templates
   howler_template_sum = howler_template_sum / 2   # 2 howler templates
-  
+
   print("-")
   print(paste("file num", file_num))
   print(paste("titi avg", titi_template_sum))
   print(paste("howler avg", howler_template_sum))
   print("-")
-  
+
   if(titi_template_sum >= detection_threshold & howler_template_sum < detection_threshold) {
     # titi detection (positive)
-    r <- tibble(original_output_file=file_name, detection="positive", avg_titi_max_score=titi_template_sum)
+    result_string <- "titi -- no howler"
+    conf_matr_entry <- 1
+  } else if(titi_template_sum >= detection_threshold & titi_template_sum > howler_template_sum) {
+    result_string <- "titi -- yes howler"
+    conf_matr_entry <- 1
   } else {
     # no detection (negative)
-    r <- tibble(original_output_file=file_name, detection="negative", avg_titi_max_score=titi_template_sum)
+    result_string <- "none"
+    conf_matr_entry <- 0
   }
+  r <- tibble(original_output_file=file_name,
+              detection=result_string,
+              avg_titi_max_score=titi_template_sum,
+              avg_howler_max_score=howler_template_sum,
+              confusion_matrix_number=conf_matr_entry
+              )
   results <- bind_rows(results, r)
   file_num = file_num + 1
 }
 
 write_csv(results, "final_output.csv")
-

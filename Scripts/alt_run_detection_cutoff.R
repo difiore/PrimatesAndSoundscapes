6710e96d164accdd8a54be8cb0b1766400278e47
Callicebus <silvyvankuijk@gmail.com>
2022-05-20 12:54:54 -0500

    Update 1.1 - 2022-05-20

--- Scripts/alt_run_detection_cutoff.R
+++ Scripts/alt_run_detection_cutoff.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(data.table)
+library(caret)
 
 
 data <- read_csv("consolidated_completedata_output.csv", col_names = TRUE)
@@ -24,4 +25,14 @@ sum(output$detection2)
 setnames(data,'species','AutomatedDetections')
 setnames(data,'PrimateSpecies','ManualDetections')
 
+# Next steps: create confusion matrix. We want to know how many false-positives,
+  # false-negatives, true-positives and true-negatives we have. Use 'caret' package.
+  # https://www.journaldev.com/46732/confusion-matrix-in-r
 
+# 1. Turn data in 'AutomatedDetections' column into 1 and 0 values (1 = detection, 0 = no detection)
+# 2. Do same for 'ManualDetections' column.
+# 3. Create confusion matrix following code in link below.
+
+# 4. Not change all manual detections with distance 4 to 0 values in
+  # 'ManualDetections' column so we can assess accuracy of model on only closer calls.
+# 5. Create new confusion matrix.

c1ed6b93ca6bc0904c7db0f3e9bd715643cdcf5d
Callicebus <silvyvankuijk@gmail.com>
2022-07-07 17:55:03 +0200

    Updating BinaryPointMatching_AD.R script. Adding template 11.

--- Scripts/BinaryPointMatching.R
+++ Scripts/BinaryPointMatching.R
@@ -1,5 +1,5 @@
-#Automated Detection Algorithm - Template Matching using Monitor's Spectrogram Cross Correlation
-#Authors: Silvy van Kuijk & Sun O'Brien
+#Automated Detection Algorithm - Template Matching using MonitoR's Binary Point Matching
+#Authors: Silvy van Kuijk, Sun O'Brien & Tony Di Fiore
 
 
 #-----------
@@ -15,81 +15,70 @@ if(install) {
 }
 
 #Call the package into R's memory:
+
 library(monitoR)
 library(tuneR)
 library(foreach)
 
 
-#List all files in dorectory
+#List all files in directory
+
 fileList <- list.files(path = "AutomatedDetection_CreatingTemplates", full.names = TRUE)
 
-#Read in .wav file and create templates
-sunwave <- readWave(fileList[1])
-template1b <- makeBinTemplate(fileList[1], frq.lim = c(0.3, 1.6), t.lim = c(25, 35), name = "F1,025,-25,T", amp.cutoff = (-25))
+# Read in .wav file and create templates... here, files on which templates are based are hard coded to prevent indexing problems...
+template1b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_001.wav", frq.lim = c(0.3, 1.6), t.lim = c(25, 35), name = "F1b,025,-25,T", amp.cutoff = (-25))
 
-sunwave <- readWave(fileList[2])
-template2b1 <- makeBinTemplate(fileList[2], frq.lim = c(0.2, 1.8), t.lim = c(0, 10), name = "F2,000,-25,T", amp.cutoff = (-25))
-template2b2 <- makeBinTemplate(fileList[2], frq.lim = c(0.2, 1.8), t.lim = c(87, 97), name = "F2,087,-25,T", amp.cutoff = (-25))
+template2b1 <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_002.wav", frq.lim = c(0.2, 1.8), t.lim = c(0, 10), name = "F2b,000,-25,T", amp.cutoff = (-25))
+template2b2 <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_002.wav", frq.lim = c(0.2, 1.8), t.lim = c(87, 97), name = "F2b,087,-25,T", amp.cutoff = (-25))
 
-sunwave <- readWave(fileList[3])
-template3b <- makeBinTemplate(fileList[3], frq.lim = c(0.2, 1.8), t.lim = c(0, 10), name = "F3,000,-25,T", amp.cutoff = (-25))
+template3b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_003.wav", frq.lim = c(0.2, 1.8), t.lim = c(0, 10), name = "F3b,000,-25,T", amp.cutoff = (-25))
 
-sunwave <- readWave(fileList[4])
-template4b1 <- makeBinTemplate(fileList[4], frq.lim = c(0.2, 1.4), t.lim = c(0, 10), name = "F4,000,-25,T", amp.cutoff = (-25))
-template4b2 <- makeBinTemplate(fileList[4], frq.lim = c(0.2, 1.8), t.lim = c(30, 40), name = "F4,030,-25,T", amp.cutoff = (-25))
+template4b1 <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_004.wav", frq.lim = c(0.2, 1.4), t.lim = c(0, 10), name = "F4b,000,-25,T", amp.cutoff = (-25))
+template4b2 <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_004.wav", frq.lim = c(0.2, 1.8), t.lim = c(30, 40), name = "F4b,030,-25,T", amp.cutoff = (-25))
 
-sunwave <- readWave(fileList[5])
-template5b <- makeBinTemplate(fileList[5], frq.lim = c(0.2, 1.4), t.lim = c(65, 75), name = "F5,065,-25,T", amp.cutoff = (-25))
+template5b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_005.wav", frq.lim = c(0.2, 1.4), t.lim = c(65, 75), name = "F5b,065,-25,T", amp.cutoff = (-25))
 
-sunwave <- readWave(fileList[6])
-template6b <- makeBinTemplate(fileList[6], frq.lim = c(0.2, 1.0), t.lim = c(15, 25), name = "F6,015,-25,H", amp.cutoff = (-25))
+template6b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_006.wav", frq.lim = c(0.2, 1.0), t.lim = c(15, 25), name = "F6b,015,-25,H", amp.cutoff = (-25))
 
-sunwave <- readWave(fileList[7])
-template7b <- makeBinTemplate(fileList[7], frq.lim = c(0.2, 1.4), t.lim = c(17, 27), name = "F7,017,-25,H", amp.cutoff = (-25))
+template7b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_007.wav", frq.lim = c(0.2, 1.4), t.lim = c(17, 27), name = "F7b,017,-25,H", amp.cutoff = (-25))
 
-sunwave <- readWave(fileList[8])
-template8b1 <- makeBinTemplate(fileList[8], frq.lim = c(0.3, 1.5), t.lim = c(25, 35), name = "F8,025,-25,T", amp.cutoff = (-25))
-template8b2 <- makeBinTemplate(fileList[8], frq.lim = c(0.3, 1.5), t.lim = c(36, 46), name = "F8,036,-25,T", amp.cutoff = (-25))
+template8b1 <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_008.wav", frq.lim = c(0.3, 1.5), t.lim = c(25, 35), name = "F8b,025,-25,T", amp.cutoff = (-25))
+template8b2 <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_008.wav", frq.lim = c(0.3, 1.5), t.lim = c(36, 46), name = "F8b,036,-25,T", amp.cutoff = (-25))
 
-sunwave <- readWave(fileList[9])
-templateManual <- makeBinTemplate(fileList[9], frq.lim = c(0.2, 1.8), t.lim = c(102, 112), name = "F9,102,-25,T", amp.cutoff = (-25))
+template0b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/ManualRecording3_REC0083.wav", frq.lim = c(0.2, 1.8), t.lim = c(102, 112), name = "F0b,102,-25,T", amp.cutoff = (-25))
 
-#Create template bin
-ctemps_adjusted_amp <- combineBinTemplates(template1b, template2b1, template2b2, template3b, template4b1, template4b2, template5b, template8b1, template8b2, template6b, template7b, templateManual)
+template9b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_009.wav", frq.lim = c(0.5, 1.0), t.lim = c(1, 7), name = "F9b,001,-25,S", amp.cutoff = (-25))
 
+template10b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_010.wav", frq.lim = c(0.5, 1.0), t.lim = c(2, 9), name = "F10b1,002,-25,S", amp.cutoff = (-25))
+
+template11b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_011.wav", frq.lim = c(0.7, 1.2), t.lim = c(10, 22), name = "F11b,010,-25,S", amp.cutoff = (-25))
+
+
+# Create a combined template bin. DON'T RUN BOTH LINES BELOW. CHOOSE WHICH ONE IS APPROPRIATE!
+
+# All templates
+ctemps_adjusted_amp <- combineBinTemplates(template1b, template2b1, template2b2, template3b, template4b1, template4b2, template5b, template6b, template7b, template8b1, template8b2, template0b, template9b, template10b, template11b)
+
+# Only spider templates
+ctemps_spiders <- combineBinTemplates(template9b, template10b, template11b)
 
 # Then, run template against all files in specified folder, from 5:45 am to 8:00 am.
 # (we manually logged detections for only those times)
 
 
-folderList <- list.files(path = "For Sun - Full test dataset/Puma 2015/0, 800", full.names = TRUE)
-outputList <- list.files(path = "For Sun - Full test dataset/Puma 2015/0, 800", full.names = TRUE)
-print(outputList[1])
-
-audioFileList <- list.files(path = outputList[1], full.names = TRUE)
-print(audioFileList[1])
-
-
-folderCounter = 1
-maxFilesPerFolder = 10   # we need to stop at 8:15 am exclusive
-
-sink(file = "output_Puma2015_0,800.txt")
-
-for(folder in folderList) {
-  fileCounter = 1
-  audioFileList <- list.files(path = folderList[folderCounter], full.names = TRUE)
-  print(paste("folder", folderCounter))
-  while(fileCounter <= maxFilesPerFolder) {
-    print(paste("    file", fileCounter))
-    scores <- binMatch(audioFileList[fileCounter], ctemps_adjusted_amp, quiet = TRUE)
-    print(scores)
-    fileCounter = fileCounter + 1
-  }
-  folderCounter = folderCounter + 1
+fileList <- list.files(path = 'E:/For Sun - Full test dataset/TestData', full.names = TRUE)
+for(value in fileList){
+  cscores <- binMatch(value, ctemps_adjusted_amp)
+  print(cscores)
+  # code for showing peaks on plot. will cause loop to not run fully, as plot(cdetects) is interactive.
+  cdetects <- findPeaks(cscores)
+  plot(cdetects)
+  showPeaks(detection.obj = cdetects, which.one = "F1,025,-21,T", point = TRUE, what = "peaks", scorelim = c(5,10))
+  print(cscores)
 }
+showPeaks()
 
-sink()
-
+#---
 
 folderList <- list.files(path = "For Sun - Full test dataset/Puma 2015/1, 200", full.names = TRUE)
 outputList <- list.files(path = "For Sun - Full test dataset/Puma 2015/1, 200", full.names = TRUE)
--- Scripts/BinaryPointMatching_AD.R
+++ Scripts/BinaryPointMatching_AD.R
@@ -13,56 +13,77 @@ if(install) {
   install.packages("monitoR")
   install.packages("tuneR")
   install.packages("foreach")
+  install.packages("tidyverse")
+  install.packages("libridate")
 }
 
 #Call the package into R's memory:
 library(monitoR)
 library(tuneR)
 library(foreach)
+library(tidyverse)
+library(lubridate)
 
 # Get a list of all files in AutomatedDetection_CreatingTemplates directory...
 fileList <- list.files(path = "AutomatedDetection_CreatingTemplates", full.names = TRUE)
 
 # Read in .wav file and create templates... here, files on which templates are based are hard coded to prevent indexing problems...
-# Note that `sunwave <-` lines are unecessary for template creation
-sunwave <- readWave("AutomatedDetection_CreatingTemplates/TestSample_001.wav")
 template1b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_001.wav", frq.lim = c(0.3, 1.6), t.lim = c(25, 35), name = "F1b,025,-25,T", amp.cutoff = (-25))
 
-sunwave <- readWave("AutomatedDetection_CreatingTemplates/TestSample_002.wav")
 template2b1 <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_002.wav", frq.lim = c(0.2, 1.8), t.lim = c(0, 10), name = "F2b,000,-25,T", amp.cutoff = (-25))
 template2b2 <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_002.wav", frq.lim = c(0.2, 1.8), t.lim = c(87, 97), name = "F2b,087,-25,T", amp.cutoff = (-25))
 
-sunwave <- readWave("AutomatedDetection_CreatingTemplates/TestSample_003.wav")
 template3b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_003.wav", frq.lim = c(0.2, 1.8), t.lim = c(0, 10), name = "F3b,000,-25,T", amp.cutoff = (-25))
 
-sunwave <- readWave("AutomatedDetection_CreatingTemplates/TestSample_004.wav")
 template4b1 <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_004.wav", frq.lim = c(0.2, 1.4), t.lim = c(0, 10), name = "F4b,000,-25,T", amp.cutoff = (-25))
 template4b2 <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_004.wav", frq.lim = c(0.2, 1.8), t.lim = c(30, 40), name = "F4b,030,-25,T", amp.cutoff = (-25))
 
-sunwave <- readWave("AutomatedDetection_CreatingTemplates/TestSample_005.wav")
 template5b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_005.wav", frq.lim = c(0.2, 1.4), t.lim = c(65, 75), name = "F5b,065,-25,T", amp.cutoff = (-25))
 
-sunwave <- readWave("AutomatedDetection_CreatingTemplates/TestSample_006.wav")
 template6b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_006.wav", frq.lim = c(0.2, 1.0), t.lim = c(15, 25), name = "F6b,015,-25,H", amp.cutoff = (-25))
 
-sunwave <- readWave("AutomatedDetection_CreatingTemplates/TestSample_007.wav")
 template7b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_007.wav", frq.lim = c(0.2, 1.4), t.lim = c(17, 27), name = "F7b,017,-25,H", amp.cutoff = (-25))
 
-sunwave <- readWave("AutomatedDetection_CreatingTemplates/TestSample_008.wav")
 template8b1 <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_008.wav", frq.lim = c(0.3, 1.5), t.lim = c(25, 35), name = "F8b,025,-25,T", amp.cutoff = (-25))
 template8b2 <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_008.wav", frq.lim = c(0.3, 1.5), t.lim = c(36, 46), name = "F8b,036,-25,T", amp.cutoff = (-25))
 
-sunwave <- readWave("AutomatedDetection_CreatingTemplates/ManualRecording3_REC0083.wav")
-templateManual <- makeBinTemplate("AutomatedDetection_CreatingTemplates/ManualRecording3_REC0083.wav", frq.lim = c(0.2, 1.8), t.lim = c(102, 112), name = "F0b,102,-25,T", amp.cutoff = (-25))
+template0b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/ManualRecording3_REC0083.wav", frq.lim = c(0.2, 1.8), t.lim = c(102, 112), name = "F0b,102,-25,T", amp.cutoff = (-25))
 
-# Create combined template bin...
-ctemps_adjusted_amp <- combineBinTemplates(template1b, template2b1, template2b2, template3b, template4b1, template4b2, template5b, template8b1, template8b2, template6b, template7b, templateManual)
+template9b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_009.wav", frq.lim = c(0.5, 1.0), t.lim = c(1, 7), name = "F9b,001,-25,S", amp.cutoff = (-25))
+
+template10b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_010.wav", frq.lim = c(0.5, 1.0), t.lim = c(2, 9), name = "F10b1,002,-25,S", amp.cutoff = (-25))
+
+template11b <- makeBinTemplate("AutomatedDetection_CreatingTemplates/TestSample_011.wav", frq.lim = c(0.7, 1.2), t.lim = c(10, 22), name = "F11b,010,-25,S", amp.cutoff = (-25))
+
+
+# Create a combined template bin. DON'T RUN BOTH LINES BELOW. CHOOSE WHICH ONE IS APPROPRIATE!
+
+# All templates
+ctemps_adjusted_amp <- combineBinTemplates(template1b, template2b1, template2b2, template3b, template4b1, template4b2, template5b, template6b, template7b, template8b1, template8b2, template0b, template9b, template10b, template11b)
+
+# Only spider templates
+ctemps_spiders <- combineBinTemplates(template9b, template10b, template11b)
+
+#---
+
+# To locate where in an audio file the high detection scores come from, use this:
+
+bscores <- binMatch("E:/For Sun - Full test dataset/Puma 2016/-2, 0/12 feb/PUMA_20160212_080000.wav", ctemps_adjusted_amp)
+bdetects <- findPeaks(bscores)
+
+bdetects # Shows you the highest detection  score for each template.
+# To find where in the audio file those were located, use the next line of code:
+plot(bdetects)
+
+
+#---
+
 
 # Tony's alternative...
-library(tidyverse)
-library(lubridate)
+
 # Get list all .wav files in the test dataset...
-audioFileList <- tibble(f = list.files(path = "~/Desktop/For Sun - Full test dataset", full.names = TRUE, recursive = TRUE)) # lists all of files recursively
+audioFileList <- data.frame(f = list.files(path = "E:/For Sun - Full test dataset", full.names = TRUE, recursive = TRUE)) # lists all of files recursively
+
 # Get a datetime for each file based on base file name in path and reformat into a datetime...
 audioFileList <- audioFileList %>% mutate(date = str_extract(basename(f),"_[0-9]+_"))
 audioFileList <- audioFileList %>% mutate(date = str_sub(date,-9, -2))
@@ -80,7 +101,7 @@ audioFileList <- audioFileList %>% mutate(location = unlist(str_split(f, "/"))[7
 
 testfiles <- 3 # just runs through a small set of .wav files to test... here, the first 3 in the dataset...
 
-outfile <- "full_output.txt"
+outfile <- "test_output.txt"
 
 sink(file = outfile)
 
